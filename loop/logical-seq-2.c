1ce1d1f898eae6aff057a6089e8a987021125cfb
hasanabill <hasanabilna@gmail.com>
2023-01-23 23:17:59 +0600

    seq 2

--- loop/logical-seq-2.c
+++ loop/logical-seq-2.c
@@ -2,13 +2,19 @@
 
 int main()
 {
-    int X, Y, i, j;
+    int X, Y, i, count = 0;
     scanf("%d%d", &X, &Y);
 
     for (i = 1; i <= Y; i++)
     {
-        printf("%d ", i);
+        count++;
+        if (count == X)
+        {
+            count = 0;
+            printf("%d\n", i);
+        }
+        else
+            printf("%d ", i);
     }
-
     return 0;
 }
