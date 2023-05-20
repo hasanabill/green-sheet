a472381f116c4a7781014f35488c1c643ed68f4b
hasanabill <hasanabilna@gmail.com>
2023-05-20 20:11:44 +0600

    onek

--- test.c
+++ test.c
@@ -2,17 +2,22 @@
 
 int main()
 {
-    int i = 0, num;
+    int N;
 
-    scanf("%d", &num);
+    scanf("%d", &N);
+    int arr[N], i;
+    arr[0] = 0;
+    arr[1] = 1;
 
-    while (i <= num)
+    for (i = 2; i < N; i++)
     {
-        i++;
-        if (num % i == 0)
-        {
-            printf("%d\n", i);
-        }
+        arr[i] = arr[i - 2] + arr[i - 1];
     }
+    for (i = 0; i < N - 1; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("%d\n", arr[N - 1]);
+
     return 0;
 }
